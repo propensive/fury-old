39b8b4c7d720cf86c9f070e8bd8c7ecab4cccfd8
Jon Pretty <jon.pretty@propensive.com>
2018-12-15 21:34:02 +0100

    Minor correction to error messages referring to parameters
    Error messages for missing parameters were including an additional `--`
    in front of each parameter.

--- src/menu/recovery.scala
+++ src/menu/recovery.scala
@@ -51,7 +51,7 @@ want to make this change to all schemas, please add the --force/-F argument.""")
       cli.abort(msg"""Could not find the file $path. Run `fury init` to create a new layer.""")
     },
     on[MissingArg].map { case MissingArg(param) =>
-      cli.abort(msg"The parameter --$param was not provided.")
+      cli.abort(msg"The parameter $param was not provided.")
     },
     on[MissingCommand].map { e =>
       cli.abort(msg"No command was provided.")
