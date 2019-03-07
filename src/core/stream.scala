dd2ac989b47aaae84a746ca8850550de431600e5
Myroslav <odisseus@users.noreply.github.com>
2019-03-07 15:59:50 +0100

    Eliminated some vars and introduced tail recursion (#283)

--- src/core/stream.scala
+++ src/core/stream.scala
@@ -15,38 +15,35 @@
  */
 package fury
 
+import scala.annotation.tailrec
 import scala.collection.immutable.Stream
 
 /** a streaming multiplexer optimized for concurrent writes */
 final class Multiplexer[K, V](keys: List[K]) {
   private[this] val state: Array[List[V]]       = Array.fill(keys.size)(Nil)
   private[this] val refs: Map[K, Int]           = keys.zipWithIndex.toMap
-  private[this] var lastSnapshot: List[List[V]] = state.to[List]
   private[this] val closed: Array[Boolean]      = Array.fill(keys.size)(false)
 
   private[this] def finished: Boolean = closed.forall(identity)
 
-  // FIXME: See if it is possible to write this as a tail-recursive method, without the
-  // `lastSnapshot` var.
   def stream(interval: Int, tick: Option[V] = None): Stream[V] = {
-    val t0       = System.currentTimeMillis
-    val snapshot = state.clone().to[List]
-    // FIXME: This could be written more efficiently with a builder
-    val changes = snapshot.zip(lastSnapshot).flatMap {
-      case (a, b) =>
-        a.take(a.length - b.length).reverse
+    @tailrec def stream(alreadyProcessed: Stream[V],lastSnapshot: List[List[V]]): Stream[V] = {
+      val t0       = System.currentTimeMillis
+      val snapshot = state.clone().to[List]
+      // FIXME: This could be written more efficiently with a builder
+      val changes = snapshot.zip(lastSnapshot).flatMap {
+        case (current, last) =>
+          current.take(current.length - last.length).reverse
+      }
+      if (finished && changes.isEmpty) {
+        alreadyProcessed ++ tick.to[Stream]
+      } else {
+        val time = System.currentTimeMillis - t0
+        if (time < interval) Thread.sleep(interval - time)
+        stream(alreadyProcessed #::: changes.to[Stream] #::: tick.to[Stream], snapshot)
+      }
     }
-    lastSnapshot = snapshot
-
-    val time = System.currentTimeMillis - t0
-    if (changes.size > 0) {
-      if (time < interval) Thread.sleep(interval - time)
-      changes.to[Stream] #::: tick.to[Stream] #::: stream(interval, tick)
-    } else if (finished) tick.to[Stream]
-    else {
-      if (time < interval) Thread.sleep(interval - time)
-      tick.to[Stream] #::: stream(interval, tick)
-    }
+    stream(Stream.empty, state.clone().to[List])
   }
 
   /** This method should only ever be called from one thread for any given reference, to
