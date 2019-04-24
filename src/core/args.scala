5d1842fa3a20b4c12437167d4e80e1ed2c8eae00
Jon Pretty <jon.pretty@propensive.com>
2019-04-24 14:27:17 +0100

    Resolve bad merge

--- src/core/args.scala
+++ src/core/args.scala
@@ -39,11 +39,6 @@ object Args {
   implicit private val ipfsRef: TExtractor[IpfsRef]     = _.headOption.flatMap(IpfsRef.parse(_))
   implicit private val reporter: TExtractor[Reporter]   = _.headOption.flatMap(Reporter.unapply(_))
   
-  implicit private val moduleId: TExtractor[ModuleId] =
-    _.headOption.flatMap(ModuleId.parse(_).toOption)
-  implicit private val projectId: TExtractor[ProjectId] =
-    _.headOption.flatMap(ProjectId.parse(_).toOption)
-
   val AllArg = CliParam[Unit]('a', 'all, "update all repositories")
 
   val BinaryRepoArg =
