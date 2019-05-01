0c10c73005632dc411c4ef2964c932a03d4db179
Jon Pretty <jon.pretty@propensive.com>
2019-05-01 15:30:59 +0100

    Add reporters and `--output` argument

---
+++ src/core/reporter.scala
@@ -0,0 +1,92 @@
+/*
+  Fury, version 0.4.0. Copyright 2018-19 Jon Pretty, Propensive Ltd.
+
+  The primary distribution site is: https://propensive.com/
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+  in compliance with the License. You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required  by applicable  law or  agreed to  in writing,  software  distributed  under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+  express  or  implied.  See  the  License for  the specific  language  governing  permissions and
+  limitations under the License.
+ */
+package fury.core
+
+import fury.io._, fury.strings._
+
+object Reporter {
+  val all: List[Reporter] = List(GraphReporter, LinearReporter, QuietReporter)
+  final private val reporters: Map[String, Reporter] = all.map { r =>
+    r.name -> r
+  }.toMap
+  def unapply(string: String): Option[Reporter] = reporters.get(string)
+  implicit val stringShow: StringShow[Reporter] = _.name
+}
+
+abstract class Reporter(val name: String) {
+
+  def report(
+      io: Io,
+      compilation: Compilation,
+      theme: Theme,
+      multiplexer: Multiplexer[ModuleRef, CompileEvent],
+      startTime: Long
+    ): Unit
+}
+
+object GraphReporter extends Reporter("graph") {
+
+  private def timeString(t: Long): String = if (t >= 10000) s"${t / 1000}s" else s"${t}ms"
+
+  def report(
+      io: Io,
+      compilation: Compilation,
+      theme: Theme,
+      multiplexer: Multiplexer[ModuleRef, CompileEvent],
+      startTime: Long
+    ): Unit = {
+    Graph.live(
+        changed = false,
+        io,
+        compilation.allDependenciesGraph.mapValues(_.to[Set]),
+        multiplexer.stream(50, Some(Tick)),
+        Map(),
+        false)(theme)
+    val duration = System.currentTimeMillis - startTime
+    io.println(msg"Total time: ${timeString(duration)}")
+  }
+}
+
+object LinearReporter extends Reporter("linear") {
+
+  def report(
+      io: Io,
+      compilation: Compilation,
+      theme: Theme,
+      multiplexer: Multiplexer[ModuleRef, CompileEvent],
+      startTime: Long
+    ): Unit =
+    multiplexer.stream(50, None).foreach {
+      case StartCompile(ref)           => io.println(msg"Starting compilation of module $ref")
+      case StopCompile(ref, true)      => io.println(msg"Successfully compiled module $ref")
+      case StopCompile(ref, false)     => io.println(msg"Compilation of module $ref failed")
+      case _ => ()
+    }
+}
+
+object QuietReporter extends Reporter("quiet") {
+
+  def report(
+      io: Io,
+      compilation: Compilation,
+      theme: Theme,
+      multiplexer: Multiplexer[ModuleRef, CompileEvent],
+      startTime: Long
+    ): Unit =
+    multiplexer.stream(50, None).foreach { event =>
+      ()
+    }
+}
