581cf7382cef2e9bad5876e43617a6506bd7350b
odisseus <odisseus@users.noreply.github.com>
2019-05-27 14:36:58 +0200

    Added Java and Scala to $PATH for bash_user.

--- etc/testshell.sh
+++ etc/testshell.sh
@@ -18,13 +18,20 @@ case "$SHELL_NAME" in
 esac
 
 apt-get install -y "${SHELL_NAME}"
-HOME="/home/${SHELL_NAME}_user"
-USER="${SHELL_NAME}_user"
-useradd -s "$(which ${SHELL_NAME})" -d "$HOME" "$USER"
-mkdir -p "$HOME"
-chown "$USER" "$HOME"
-git config --global user.email "${USER}@${USER}"
-git config --global user.name "${USER}"
-su "$USER" -c "touch $HOME/.${SHELL_NAME}rc"
-su "$USER" -c "${SHELL_NAME}" -c /install.sh
-su "$USER" -c "${SHELL_NAME}" -c "source /home/${SHELL_NAME}_user/.${SHELL_NAME}rc ${JOIN} fury start ${JOIN} fury about ${JOIN} fury stop"
+NEW_HOME="/home/${SHELL_NAME}_user"
+NEW_USER="${SHELL_NAME}_user"
+useradd -s "$(which ${SHELL_NAME})" -d "$NEW_HOME" "$NEW_USER"
+mkdir -p "$NEW_HOME"
+cat <<EOF >"$NEW_HOME/commands"
+echo "Hello from $NEW_USER"
+env | grep PATH
+git config --global user.email "$NEW_USER@$NEW_USER"
+git config --global user.name "$NEW_USER"
+touch "$NEW_HOME/.${SHELL_NAME}rc"
+"$SHELL_NAME" /install.sh
+"$SHELL_NAME" source /home/${SHELL_NAME}_user/.${SHELL_NAME}rc ${JOIN} fury start ${JOIN} fury about ${JOIN} fury stop
+EOF
+chown -R "$NEW_USER" "$NEW_HOME"
+chmod +x "$NEW_HOME/commands"
+
+su -l "$NEW_USER" -c "${SHELL_NAME}" -c "$NEW_HOME/commands"
