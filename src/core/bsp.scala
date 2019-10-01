45e76f21b4672cbec049c0d3bf3b588631ac5372
Jon Pretty <jon.pretty@propensive.com>
2019-10-01 16:09:37 +0100

    Use new import format
    This will break all existing builds

--- src/core/bsp.scala
+++ src/core/bsp.scala
@@ -120,7 +120,7 @@ class FuryBuildServer(layout: Layout, cancel: Cancelator, https: Boolean, instal
       focus          <- Ogdl.read[Focus](layout.focusFile, identity(_))
       layer          <- ~Layer.read(io, focus.layerRef, layout, globalLayout)
       schema         <- layer.mainSchema
-      hierarchy      <- schema.hierarchy(io, layout.pwd, layout, globalLayout, https)
+      hierarchy      <- schema.hierarchy(io, layout, globalLayout, https)
       universe       <- hierarchy.universe
       projects       <- layer.projects
       graph          <- projects.flatMap(_.moduleRefs).map { ref =>
