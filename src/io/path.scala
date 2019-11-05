4fd53e1475b63e15bf75b1d4ea78ec6662c48309
Jon Pretty <jon.pretty@propensive.com>
2019-11-05 12:14:41 +0000

    Using cached binaries

--- src/io/path.scala
+++ src/io/path.scala
@@ -80,7 +80,9 @@ case class Path(value: String) {
       }
   }
 
-  def hardLink(path: Path): Try[Unit] = Try(Files.createLink(javaPath, path.javaPath))
+  def hardLink(path: Path): Try[Unit] = Try(Files.createLink(javaPath, path.javaPath)).map { _ => () }.recoverWith {
+    case ex: java.nio.file.NoSuchFileException => copyTo(path).map { _ => () }
+  }
 
   def touch(): Try[Unit] = Outcome.rescue[java.io.IOException](FileWriteError(this)) {
     if(!exists()) new java.io.FileOutputStream(javaFile).close()
