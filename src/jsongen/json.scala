322ed846ebda6eb9c301524678a5be26e79e73af
Jon Pretty <jon.pretty@propensive.com>
2019-11-23 10:54:03 +0000

    Updated to version 0.7.12

--- src/jsongen/json.scala
+++ src/jsongen/json.scala
@@ -1,6 +1,6 @@
 /*
    ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-   ║ Fury, version 0.7.7. Copyright 2018-19 Jon Pretty, Propensive OÜ.                                         ║
+   ║ Fury, version 0.7.12. Copyright 2018-19 Jon Pretty, Propensive OÜ.                                         ║
    ║                                                                                                           ║
    ║ The primary distribution site is: https://propensive.com/                                                 ║
    ║                                                                                                           ║
