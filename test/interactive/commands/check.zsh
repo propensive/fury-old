91c0aa681a99fcd6754aa7d33ae7674d72fa8583
Jon Pretty <jon.pretty@propensive.com>
2019-11-27 17:52:53 +0100

    Support for running "interactive" (completions" tests
    These require `tmux` to be installed. They fake a command being typed
    into a terminal in a particular shell, followed by a `tab` character,
    which should invoke tab-completion for the appropriate shell.

---
+++ test/interactive/commands/check.zsh
@@ -0,0 +1,23 @@
+about       -- about Fury
+alias       -- view and edit command aliases
+binary      -- manage binary dependencies for the module
+build       -- perform build actions
+clean       -- clean fury workspace
+config      -- change system configuration options
+dependency  -- manage dependencies for the module
+env         -- manage application environment variables
+help        -- help on using Fury
+kill        -- kill the Fury server
+layer       -- view and edit the layer
+module      -- view and edit modules
+param       -- manage compiler parameters for the module
+permission  -- manage application permissions
+project     -- manage projects
+property    -- manage application -D properties
+repo        -- manage source repositories for the schema
+restart     -- restart the Fury server
+schema      -- manage the current schema
+source      -- manage sources for the module
+stop        -- gracefully shut down the Fury server
+test        -- run the Fury tests
+upgrade     -- upgrade to the latest version of Fury
