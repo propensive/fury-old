1bacdda6eae313a0b9a9a51e5bf83fc2268ccc11
ricardoffmartins <31004151+ricardoffmartins@users.noreply.github.com>
2019-12-17 14:23:15 +0000

    The layer select command goes crazy when the layer name is wrong #782 (#786)
    Create a function to verify if a layer exists.
    New exception LayersFailure that is thrown when the given name doesn't exist.
    Change the Root ImportPath from ImportPath("") to ImportPath("/")
    
    ref: layerSelectCommand

--- src/build/build.scala
+++ src/build/build.scala
@@ -498,16 +498,24 @@ object LayerCli {
     layout    <- cli.layout
     baseLayer <- Layer.base(layout)
     schema    <- baseLayer.mainSchema
-    cli       <- cli.hint(LayerArg, ImportPath("/") :: schema.importTree(layout, true).getOrElse(Nil))
+    cli       <- cli.hint(LayerArg,  schema.importTree(layout, true).getOrElse(Nil))
     call      <- cli.call()
-    _         <- schema.importTree(layout, true)
+    layers    <- schema.importTree(layout, true)
     relPath   <- call(LayerArg)
+    -         <- verifyLayers(relPath, layers)
     focus     <- Layer.readFocus(layout)
     newPath   <- ~focus.path.dereference(relPath)
     newFocus  <- ~focus.copy(path = newPath)
     _         <- Layer.saveFocus(newFocus, layout)
   } yield log.await()
- 
+
+
+  def verifyLayers(path: ImportPath, list: List[ImportPath]): Try[Unit] =
+    if (list.map(_.path).contains(path.path))
+      Success()
+    else
+      Failure(LayersFailure(path))
+
   def extract(cli: Cli[CliParam[_]])(implicit log: Log): Try[ExitStatus] = for {
     cli      <- cli.hint(DirArg)
     cli      <- cli.hint(FileArg)
