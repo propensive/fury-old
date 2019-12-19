2c5cdd3c9b6b69a502d5448c46109115feea3c17
Jon Pretty <jon.pretty@propensive.com>
2019-12-19 16:14:24 +0100

    If the current project name changes, keep pointing at it (#800)
    * If current project is renamed, keep pointing to it
    
    * If no project selected during update, choose that project

--- src/project/project.scala
+++ src/project/project.scala
@@ -148,8 +148,9 @@ object ProjectCli {
       nameArg        <- ~call(ProjectNameArg).toOption
       newId          <- ~nameArg.flatMap(schema.unused(_).toOption)
       layer          <- focus(layer, _.lens(_.projects(on(project.id)).id)) = newId
+      layer          <- if(!schema.main.isEmpty) ~layer else focus(layer, _.lens(_.main)) = Some(newId)
+      layer          <- if(Some(project.id) != schema.main) ~layer else focus(layer, _.lens(_.main)) = Some(newId)
       newSchema      <- layer.schemas.findBy(optSchemaId.getOrElse(layer.main))
-      newSchema      <- ~(if(Some(project.id) == newSchema.main) newSchema.copy(main = newId) else newSchema)
       lens           <- ~Lenses.layer.schemas
       layer          <- ~lens.modify(layer)(_.filterNot(_.id == schema.id))
       layer          <- ~lens.modify(layer)(_ + newSchema)
