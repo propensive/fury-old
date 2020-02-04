53e21d9b10b74128173bdf117ec7e981223aab62
odisseus <odisseus@users.noreply.github.com>
2020-02-04 16:49:28 +0100

    Moved teeoutputstream.scala to the io module.

--- src/io/teeoutputstream.scala
+++ src/io/teeoutputstream.scala
@@ -14,7 +14,7 @@
    ║ See the License for the specific language governing permissions and limitations under the License.        ║
    ╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 */
-package fury.utils
+package fury.io
 
 import java.io._
 import scala.util._
