c1e881a8523a6b7e20ee0d45938fcc28057f62f6
Jon Pretty <jon.pretty@propensive.com>
2020-04-21 10:04:42 +0200

    Fixed header revision script, and reinstated `Fury` object

--- src/frontend/fury.scala
+++ src/frontend/fury.scala
@@ -14,3 +14,8 @@
     See the License for the specific language governing permissions and limitations under the License.
 
 */
+import com.facebook.nailgun
+
+object Fury {
+  def main(args: Array[String]): Unit = NGServer.main(args)
+}
