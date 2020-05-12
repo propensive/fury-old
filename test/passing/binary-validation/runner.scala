a4ab032f007d41a218f513db1f6c8bead8ad0fe5
Andrew Stefanich <andrewstefanich@gmail.com>
2020-05-12 16:15:19 -0400

    test corrected for commons dependency

--- test/passing/binary-validation/runner.scala
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.io.IOCase
-import org.apache.commons.math3.util.BigReal
-
-object Runner {
-
-  val ioDependency = IOCase.SYSTEM
-
-  val mathDependency = BigReal.ONE
-}
---
+++ test/passing/binary-validation/src/runner.scala
@@ -0,0 +1,7 @@
+import org.apache.commons.io.IOCase
+import org.apache.commons.math3.util.BigReal
+
+object Runner extends App {
+  println(IOCase.SYSTEM)
+  println(BigReal.ONE)
+}
