d3aa59074652575b4504551b8928137163494336
Jon Pretty <jon.pretty@propensive.com>
2020-08-08 14:50:31 +0200

    Updated classes-include layer

--- test/passing/classes-include/src/print/main.scala
+++ test/passing/classes-include/src/print/main.scala
@@ -5,5 +5,5 @@ import java.io._
 object Main extends App {
   println("Running print module")
   val dir = new File("classfiles")
-  dir.listFiles().foreach(println)
+  dir.listFiles().map(_.toString).sorted.foreach(println)
 }
