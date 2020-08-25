4258da2c5c79090587bbef6ce644955a83ec94c1
Jon Pretty <jon.pretty@propensive.com>
2020-08-25 14:28:35 +0200

    Updated source headers to version 0.18.8

--- src/core/context.scala
+++ src/core/context.scala
@@ -1,6 +1,6 @@
 /*
 
-    Fury, version 0.18.7. Copyright 2018-20 Jon Pretty, Propensive OÜ.
+    Fury, version 0.18.8. Copyright 2018-20 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
